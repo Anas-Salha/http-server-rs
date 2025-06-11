68c730640957e6e0b9257a075fe4136f59cc7e09
Anas Salha <anassalha96@gmail.com>
2025-06-11 21:41:44 +0200

    Add error handling to read_request

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,23 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let req = read_request(&mut stream);
-                let resp = req.execute_method();
+                match read_request(&mut stream) {
+                    Ok(req) => {
+                        let resp = req.execute_method();
 
-                // Respond
-                let msg = resp.to_string();
-                if let Err(e) = stream.write_all(msg.as_bytes()) {
-                    eprintln!("Failed to write to stream: {}", e);
-                };
-                if let Err(e) = stream.flush() {
-                    eprintln!("Failed to flush stream: {}", e);
-                };
+                        // Respond
+                        let msg = resp.to_string();
+                        if let Err(e) = stream.write_all(msg.as_bytes()) {
+                            eprintln!("Failed to write to stream: {}", e);
+                        };
+                        if let Err(e) = stream.flush() {
+                            eprintln!("Failed to flush stream: {}", e);
+                        };
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to read request: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -38,21 +44,24 @@ fn main() {
 //
 // If a message body has been indicated,
 // then it is read as a stream until an amount of octets equal to the message body length is read or the connection is closed.
-fn read_request(stream: &mut TcpStream) -> HttpRequest {
+fn read_request(stream: &mut TcpStream) -> Result<HttpRequest, Box<dyn std::error::Error>> {
     // Setup buffer to read stream
     let mut buf = [0u8; 4096];
-    let _ = stream.read(&mut buf).unwrap(); // expect the line in a single read
+    stream.read(&mut buf)?;
 
     // 1. Read request line
-    let line_end = buf.windows(2).position(|w| w == b"\r\n").unwrap();
-    let line = std::str::from_utf8(&buf[..line_end]).unwrap();
+    let line_end = buf
+        .windows(2)
+        .position(|w| w == b"\r\n")
+        .ok_or("Failed to find request line end")?;
+    let line = std::str::from_utf8(&buf[..line_end])?;
 
     let mut fields = line.split_whitespace();
-    let method = fields.next().unwrap().parse().unwrap();
-    let target = fields.next().unwrap().to_owned();
-    let version = fields.next().unwrap().parse().unwrap();
+    let method = fields.next().ok_or("Failed to parse method")?.parse()?;
+    let target = fields.next().ok_or("Failed to parse target")?.to_owned();
+    let version = fields.next().ok_or("Failed to parse version")?.parse()?;
 
-    HttpRequest::new(method, target, version)
+    Ok(HttpRequest::new(method, target, version))
 
     // 2. Read headers
     // 3. Read (optional) body
